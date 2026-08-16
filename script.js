9020f761311410a81737dae949639725c69f6837
Caraale07 <mohamoddahir11@gmail.com>
2026-08-16 18:03:03 +0100

    fix firebase

--- script.js
+++ script.js
@@ -1,71 +1,33 @@
-const button = document.querySelector(".hero button");
-button.addEventListener("click", function() {
-    alert("Thanks for visiting my website");
-});
+console.log("SCRIPT WAA SHAQAYNAYAA");
+
+const clickMe = document.getElementById("clickMe");
 
 const startBtn = document.getElementById("startBtn");
 
-startBtn.addEventListener("click", function() {
-    document.querySelector(".about").scrollIntoView( {
-
-    });
-});
-
 const contactform = document.getElementById("contactform");
 
-contactform.addEventListener("submit", function(event) {event.preventDefault();
-    alert("Thank you! Your message has been sent.");
-});
- import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
- 
- import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+clickMe.addEventListener("click", function () {
 
-const firebaseConfig = {
-apiKey: "AIzaSyB5nu0Ctzc7r1utYIYqxQ6AU8lRty0Z_g0",
- authDomain: "somali-wayn-e1713.firebaseapp.com",
- projectId: "somali-wayn-e1713",
- storageBuckey: "somali-wayn-e1713.firebasestorage.app",
- messagingSenderId: "246449590312",
- appId: "1:246449590312:web:422275aff3058caf63e51d"
-};
+  alert("Thanks for visiting my website!");
 
-const app = inlitializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-const signUpBtn = document.getElementById("signUpBtn");
-const loginBtn = document.getElementById("loginBtn");
-
-const email = document.getElementById("authEmail");
-const password = document.getElementById("authPassword");
-const authMessage = document.getElementById("authMessage");
-
-loginBtn.addEventListener("click", async function () {
-    const { error } = await signInWithEmailAndPassword(
-        auth,
-        email.value,
-        password.value,
-    );
-    if (error) {
-        authMessage.textContent = error.message;
-    } else {
-        authMessage.textContent = "login successfully";
-    }
 });
 
-signUpBtn.addEventListener("click", async function () {
-    try {
-        awiat createUserWithEmailAndPassword(
-            auth,
-            email.value,
-            password.value,
-        );
+startBtn.addEventListener("click", function () {
 
-        authMessage.textContent = "Account created successfully!";
-    } catch (error) {
-        authMessage.textContent = error.messge;
-    }
+  document.getElementById("about").scrollIntoView({
+
+    behavior: "smooth"
+
+  });
+
+});
+
+contactform.addEventListener("submit", function (event) {
+
+  event.preventDefault();
+
+  alert("Thank you! Your message has been sent.");
+
+  contactform.reset();
+
 });
