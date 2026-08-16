01987cd880a9f52bd852d18931654e5245b315d5
Caraale07 <mohamoddahir11@gmail.com>
2026-08-16 16:24:55 +0100

    fix firebase

--- script.js
+++ script.js
@@ -16,40 +16,56 @@ const contactform = document.getElementById("contactform");
 contactform.addEventListener("submit", function(event) {event.preventDefault();
     alert("Thank you! Your message has been sent.");
 });
-https://kueucpqxmbkmfnvkcxsx.supabase.co
-sb_publishable_iQl9IcX9q77Lpo1yXhe-HQ_jdYhmXwi
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+ 
+ import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyB5nu0Ctzc7r1utYIYqxQ6AU8lRty0Z_g0",
+ authDomain: "somali-wayn-e1713.firebaseapp.com",
+ projectId: "somali-wayn-e1713",
+ storageBuckey: "somali-wayn-e1713.firebasestorage.app",
+ messagingSenderId: "246449590312",
+ appId: "1:246449590312:web:422275aff3058caf63e51d"
+};
+
+const app = inlitializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 const signUpBtn = document.getElementById("signUpBtn");
 const loginBtn = document.getElementById("loginBtn");
+
+const email = document.getElementById("authEmail");
+const password = document.getElementById("authPassword");
 const authMessage = document.getElementById("authMessage");
 
-signUpBtn = addEventListener("click", async function () {
-    const email = document.getElementById("authEmail").value;
-    const password = document.getElementById("authPassword").value;
-
-    const { error } = await supabaseClient.auth.signUp({
-        email: email,
-        password: password
-    });
-
+loginBtn.addEventListener("click", async function () {
+    const { error } = await signInWithEmailAndPassword(
+        auth,
+        email.value,
+        password.value,
+    );
     if (error) {
-        authMessage. textContent = errorr.message; 
+        authMessage.textContent = error.message;
     } else {
-        authMessage. textCntent = "Account created successfully!";
+        authMessage.textContent = "login successfully";
     }
 });
 
-loginBtn.addEventListener("click", async function () {
-    const email = document.getElementById("authEmail").value;
-    const password = document.getElementById("authPassword").value;
+signUpBtn.addEventListener("click", async function () {
+    try {
+        awiat createUserWithEmailAndPassword(
+            auth,
+            email.value,
+            password.value,
+        );
 
-    const { error } = await supabaseclient.auth.signInwithPassword({
-        email: email,
-        password: password
-    });
-
-    if (error) {
-        authMessage. textContent = error.message;
-    } else {
-        authMessage. textContent = "Login successfully!";
+        authMessage.textContent = "Account created successfully!";
+    } catch (error) {
+        authMessage.textContent = error.messge;
     }
 });
